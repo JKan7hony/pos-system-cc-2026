812f9c8c86eb8a73b838a4a180c1d4b412ecc840
Joaquin Antonio Muñoz Hermosilla <joaquin.munoz3@virginiogomez.cl>
2026-06-15 17:59:45 -0400

    Añadir validaciones extra en cliente y productos

--- backend/src/validators/productValidator.js
+++ backend/src/validators/productValidator.js
@@ -9,14 +9,10 @@ const createProductValidation = [
         .withMessage('El nombre debe tener entre 3 y 100 caracteres'),
 
     body('precio')
-        .notEmpty()
-        .withMessage('El precio es obligatorio')
         .isFloat({ min: 0 })
         .withMessage('El precio debe ser mayor o igual a 0'),
 
     body('stock')
-        .notEmpty()
-        .withMessage('El stock es obligatorio')
         .isInt({ min: 0 })
         .withMessage('El stock debe ser mayor o igual a 0'),
 
@@ -33,12 +29,12 @@ const updateProductValidation = [
         .isLength({ min: 3, max: 100 }),
 
     body('precio')
-        .optional()
-        .isFloat({ min: 0 }),
+        .isFloat({ min: 0 })
+        .withMessage('El precio debe ser mayor o igual a 0'),
 
     body('stock')
-        .optional()
-        .isInt({ min: 0 }),
+        .isInt({ min: 0 })
+        .withMessage('El stock debe ser mayor o igual a 0'),
 
     body('categoria_id')
         .optional()
